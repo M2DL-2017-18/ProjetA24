fe53ab8bae16e97387678e913f7f6599e4bc1ce8
Rauffet Guillaume <guillaume.rauffet@gmail.com>
2018-01-25 22:58:57 +0100

    Javadoc INFRA 1st step

--- ProjetINFRA/src/main/java/fr/m2dl/infra/Orchestrator.java
+++ ProjetINFRA/src/main/java/fr/m2dl/infra/Orchestrator.java
@@ -3,19 +3,34 @@ package fr.m2dl.infra;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * Scheduler of the Multi-Agent System
+ *
+ * It "runs" all the agents in the system
+ */
 public class Orchestrator {
     List<ActiveEntity> activeEntityList;
     List<PassiveEntity> passiveEntityList;
     List<Agent> agentList;
 
+    /**
+     * Default constructor
+     */
     Orchestrator() {
         agentList = new ArrayList<Agent>();
     }
 
+    /**
+     * Adds an agent to the system
+     * @param a the agent to add
+     */
     public void createAgent(Agent a) {
         agentList.add(a);
     }
 
+    /**
+     * Runs all the agent lifecycle sequentially
+     */
     public void run() {
         for (Agent a : agentList) {
             a.runLifeCycle();
