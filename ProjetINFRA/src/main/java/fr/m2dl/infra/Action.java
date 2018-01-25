80aaf2310778eed38896d4a8d08dace8c9d47d10
Rauffet Guillaume <guillaume.rauffet@gmail.com>
2018-01-25 22:58:57 +0100

    Javadoc INFRA 1st step

--- ProjetINFRA/src/main/java/fr/m2dl/infra/Action.java
+++ ProjetINFRA/src/main/java/fr/m2dl/infra/Action.java
@@ -1,5 +1,11 @@
 package fr.m2dl.infra;
 
+/**
+ * Describes an action that an agent can make
+ */
 public abstract class Action {
+    /**
+     * The concrete action
+     */
     public abstract void act();
 }
--- ProjetINFRA/src/main/java/fr/m2dl/infra/Agent.java
+++ ProjetINFRA/src/main/java/fr/m2dl/infra/Agent.java
@@ -3,16 +3,33 @@ package fr.m2dl.infra;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * Describes what is an agent and what it supposed to do
+ */
 public abstract class Agent {
     private List<Action> actionList;
 
+    /**
+     * Default constructor
+     */
     public Agent() {
         actionList = new ArrayList<Action>();
     }
 
+    /**
+     * An agent can perceive
+     */
     public abstract void sense();
+
+    /**
+     * An agent can decide what will be it's next action
+     * @return the next action to do
+     */
     public abstract Action decide();
 
+    /**
+     * An agent has a lifecycle : perceive, decide, act
+     */
     protected void runLifeCycle() {
         sense();
         Action a = decide();
