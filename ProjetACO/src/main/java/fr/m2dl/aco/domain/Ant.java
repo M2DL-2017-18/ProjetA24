0b47384cfddec63dd6dcba14c525894f56457da5
benBubbleflat <benjamin@bubbleflat.com>
2018-01-25 16:40:45 +0100

    #21 fix typo

--- ProjetACO/src/main/java/fr/m2dl/aco/domain/Ant.java
+++ ProjetACO/src/main/java/fr/m2dl/aco/domain/Ant.java
@@ -5,8 +5,8 @@ import java.util.logging.Logger;
 public class Ant {
 
     private final static Logger logger = Logger.getLogger(Ant.class.getSimpleName());
-    public Ant() {
 
+    public Ant() {
         logger.info("je suis une fourmi.");
     }
 }
