b3576cdf14a5163c7e44d1b31adfa363f7506fb9
FontBenjamin <font.benjamin@yahoo.fr>
2018-01-25 17:37:12 +0100

    Affichage de la fourmi sur l'ecran

--- ProjetFF2D/src/main/java/fr/m2dl/ff2d/Main.java
+++ ProjetFF2D/src/main/java/fr/m2dl/ff2d/Main.java
@@ -25,7 +25,7 @@ public class Main extends Application {
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
 			primaryStage.setTitle("Ant Simulation");
-			//primaryStage.show();
+			primaryStage.show();
 		} catch(Exception e) {
 			e.printStackTrace();
 		}
