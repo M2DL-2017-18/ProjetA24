e675cb8e3fb0287e22c54fe204bb4a1cbe62ad69
FontBenjamin <font.benjamin@yahoo.fr>
2018-01-26 11:07:03 +0100

    meilleure ui responsive (gridpane)

--- ProjetFF2D/src/main/java/controller/Controller.java
+++ ProjetFF2D/src/main/java/controller/Controller.java
@@ -32,21 +32,31 @@ public class Controller {
 
 	@FXML
 	private void initialize() {
-		
-		gridPane = new GridPane();
-		gridPane.setGridLinesVisible(true);
+		// on lance la grille et on créé ses colonnes et lignes
+		this.gridPane = new GridPane();
+		this.gridPane.setGridLinesVisible(true);
 		for (int i = 0; i < numCols; i++) {
             ColumnConstraints colConst = new ColumnConstraints(32);
-            //colConst.setPercentWidth(100.0 / numCols);
+            colConst.setPercentWidth(100.0 / numCols);
+            colConst.setMinWidth(0);
             gridPane.getColumnConstraints().add(colConst);       
         }
         for (int i = 0; i < numRows; i++) {
             RowConstraints rowConst = new RowConstraints(32);
-           // rowConst.setPercentHeight(100.0 / numRows);         
+            rowConst.setPercentHeight(100.0 / numRows);
+            rowConst.setMinHeight(0);
             gridPane.getRowConstraints().add(rowConst);   
+           
         }
 		
-        gridPanel.getChildren().add(gridPane);
+        // on ajoute au panel la grille
+        this.gridPanel.getChildren().add(gridPane);
+        
+        // on redimensionne la grille pour qu'elle s'adapte a la bonne taille
+        this.gridPane.setStyle("-fx-background-color: white;");
+        this.gridPane.prefWidthProperty().bind(this.gridPanel.widthProperty());
+        this.gridPane.prefHeightProperty().bind(this.gridPanel.heightProperty());
+
 	}
 
 	public void setApp(Main main) {
@@ -62,13 +72,14 @@ public class Controller {
         logger.info("je suis une fourmi graphique.");
 		ImageView iv = new ImageView(getClass().getResource("images.png").toExternalForm());
 		this.gridPane.getChildren().get(0);
-		Label antUI = new Label("F");
+		Label antUI = new Label("     ");
 		antUI.setStyle("-fx-border-color:black; -fx-background-color: black;");
 		this.gridPane.add(antUI, x, y);
 
 	}
 
 	public void launchSimulation() {
+		// par defaut le nid est au millieux de la carte
 		this.launchAnt(numCols/2, numRows/2);
 	}
 	
