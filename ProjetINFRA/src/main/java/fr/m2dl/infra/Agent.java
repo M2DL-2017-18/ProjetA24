8030f279df9a524df6d4f8dce700a0f70a608159
NotBad4U <alessio.coltellacci@clever-cloud.com>
2018-02-01 16:42:56 +0100

    Add state for an Agent
    This state will be use to manage the internal state of the agent by the System

--- ProjetINFRA/src/main/java/fr/m2dl/infra/Agent.java
+++ ProjetINFRA/src/main/java/fr/m2dl/infra/Agent.java
@@ -5,11 +5,17 @@ import java.util.List;
 import java.util.logging.Logger;
 import java.util.UUID;
 
+enum State {
+    Live,
+    Die,
+}
+
 /**
  * Describes what is an agent and what it supposed to do
  */
 public abstract class Agent {
     private UUID id;
+    private State state;
     private List<Action> actionList;
     private final static Logger logger = Logger.getLogger(Agent.class.getSimpleName());
     private Behavior behavior;
@@ -19,6 +25,7 @@ public abstract class Agent {
      */
     public Agent(Behavior b) {
         id = UUID.randomUUID();
+        state = State.Live;
         actionList = new ArrayList<Action>();
         this.behavior = b;
         logger.info("Création d'un agent");
@@ -41,5 +48,17 @@ public abstract class Agent {
 
     public UUID getId() {
         return this.id;
+    }
+
+    public State getState() {
+        return this.state;
+    }
+
+    public void setState(State s) {
+        this.state = s;
+    }
+
+    private void suicide() {
+        this.state = State.Die;
     }
 }
