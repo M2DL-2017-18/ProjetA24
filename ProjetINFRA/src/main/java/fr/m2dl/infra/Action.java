1bf04f570ef942d1e211ef21eac50c4c4ccd265c
kladier <kevin.ladier@gmail.com>
2018-02-02 15:35:06 +0100

    [INFRA] add behavior class, test + change lifecycle

--- ProjetINFRA/src/main/java/fr/m2dl/infra/Action.java
+++ ProjetINFRA/src/main/java/fr/m2dl/infra/Action.java
@@ -3,9 +3,9 @@ package fr.m2dl.infra;
 /**
  * Describes an action that an agent can make
  */
-public abstract class Action {
+public interface Action {
     /**
      * The concrete action
      */
-    public abstract void act();
+    void act(Agent activeAgent, LocalEnv env);
 }
