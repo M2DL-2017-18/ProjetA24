b0f4825f0eebdba74701d48e12744f0ccadcc77f
Rhich <rouishichem@yahoo.fr>
2018-02-02 15:25:19 +0100

    #21 Create Environment + services

---
+++ ProjetACO/src/main/java/fr/m2dl/aco/domain/Box.java
@@ -0,0 +1,5 @@
+package fr.m2dl.aco.domain;/**
+ * Created by hichem on 02/02/2018.
+ */
+public class Box {
+}
---
+++ ProjetACO/src/main/java/fr/m2dl/aco/services/IBoxable.java
@@ -0,0 +1,11 @@
+package fr.m2dl.aco.services;
+
+import javafx.util.Pair;
+
+/**
+ * Created by hichem on 02/02/2018.
+ */
+public interface IBoxable {
+    Coordinates getCoordinates();
+    void setCoordinates(Coordinates coordinates);
+}
