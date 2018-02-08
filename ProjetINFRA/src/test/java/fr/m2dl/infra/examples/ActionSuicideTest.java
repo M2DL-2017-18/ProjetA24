c2b3a489383dcf583fa19aa81706591d5d80216f
kladier <kevin.ladier@gmail.com>
2018-02-08 16:27:25 +0100

    generic localEnv and create active entity in orchestrator

--- ProjetINFRA/src/test/java/fr/m2dl/infra/examples/ActionSuicideTest.java
+++ ProjetINFRA/src/test/java/fr/m2dl/infra/examples/ActionSuicideTest.java
@@ -4,7 +4,7 @@ import fr.m2dl.infra.Action;
 import fr.m2dl.infra.Agent;
 import fr.m2dl.infra.LocalEnv;
 
-public class ActionSuicideTest implements Action<Agent> {
+public class ActionSuicideTest implements Action<Agent, LocalEnv> {
 
     @Override
     public void act(Agent activeAgent, LocalEnv env) {
