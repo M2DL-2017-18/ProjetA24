c2e76a742686f1b3f89a8ed2ed102c045aa31d6a
MKostiuk <mathieukostiuk@hotmail.fr>
2018-02-09 23:53:34 +0100

    Implémentation recherche nourriture fourmi environnement + tests unitaires

--- ProjetFF2D/src/main/java/fr/m2dl/ff2d/model/Behavior.java
+++ ProjetFF2D/src/main/java/fr/m2dl/ff2d/model/Behavior.java
@@ -1,12 +1,19 @@
 package fr.m2dl.ff2d.model;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+
+import java.util.stream.Collectors;
+
 
 import fr.m2dl.aco.action.move.MoveBottom;
 import fr.m2dl.aco.action.move.MoveLeft;
 import fr.m2dl.aco.action.move.MoveRight;
 import fr.m2dl.aco.domain.Ant;
+import fr.m2dl.aco.domain.Box;
+import fr.m2dl.aco.domain.Coordinates;
+import fr.m2dl.aco.domain.Food;
 import fr.m2dl.aco.services.IAction;
 import fr.m2dl.aco.services.IBehavior;
 import fr.m2dl.aco.services.IEnvironment;
@@ -20,6 +27,7 @@ import fr.m2dl.infra.Action;
  */
 
 public class Behavior implements IBehavior{
+	
 	
 	
 	
@@ -40,9 +48,24 @@ public class Behavior implements IBehavior{
 		
 		listeAction.addAll(uTurn());
 		
+		
+		
+		
 		return listeAction;
 	}
 
-
-
+	public List<Coordinates> findFoodInGrid(Box[][] grid) {
+		
+		List<Coordinates> foodCoordinates = new ArrayList<Coordinates>();
+		
+		foodCoordinates = Arrays.stream(grid)
+				.flatMap(row -> Arrays.stream(row).map(column -> column.getBoxables()))
+				.flatMap(box -> box.stream())
+				.filter(elem -> elem instanceof Food)
+				.map(food -> food.getCoordinates())
+				.collect(Collectors.toList());
+		
+		return foodCoordinates;
+	}
+	
 }
---
+++ ProjetFF2D/src/main/java/fr/m2dl/ff2d/test/BehaviorTest.java
@@ -0,0 +1,65 @@
+package fr.m2dl.ff2d.test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import fr.m2dl.aco.domain.Box;
+import fr.m2dl.aco.domain.Coordinates;
+import fr.m2dl.aco.domain.Food;
+import fr.m2dl.ff2d.model.Behavior;
+
+public class BehaviorTest {
+
+	private Box[][] grid;
+	private Behavior b;
+	
+	@Before
+	public void createGrid() {
+		b = new Behavior();
+		grid = new Box[3][3];
+		
+		for (int i = 0; i < 3; i++)
+			for (int j = 0; j < 3; j++)
+				grid[i][j] = new Box();
+	}
+	
+	@Test
+	public void testFindFoodCoordinatesListNotNull() {
+		assertNotNull(b.findFoodInGrid(grid));
+	}
+	
+	@Test
+	public void testFindOneFood() {
+		Food f = new Food(new Coordinates(1,1),1);
+		grid[2][2].addBoxable(f);
+		assertEquals(b.findFoodInGrid(grid).size(),1);
+	}
+	
+	@Test
+	public void testThreeFoods() {
+		Food f1 = new Food(new Coordinates(1,1),1);
+		Food f2 = new Food(new Coordinates(0,0),1);
+		Food f3 = new Food(new Coordinates(1,0),1);
+		grid[2][2].addBoxable(f1);
+		grid[1][2].addBoxable(f2);
+		grid[0][2].addBoxable(f3);
+		assertEquals(b.findFoodInGrid(grid).size(),3);
+	}
+	
+	@Test
+	public void testFoodCoordinatesX() {
+		Food f1 = new Food(new Coordinates(1,2),1);
+		grid[1][2].addBoxable(f1);
+		assertEquals(b.findFoodInGrid(grid).get(0).getX(),1);
+	}
+	
+	@Test
+	public void testFoodCoordinatesY() {
+		Food f1 = new Food(new Coordinates(1,2),1);
+		grid[1][2].addBoxable(f1);
+		assertEquals(b.findFoodInGrid(grid).get(0).getY(),2);
+	}
+
+}
